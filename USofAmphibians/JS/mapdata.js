e02c4a7c14afcf7fc0a1e1c04e070f340643e954
loehrae <59887616+loehrae@users.noreply.github.com>
2020-06-30 20:59:39 -0500

    Add files via upload

---
+++ USofAmphibians/JS/mapdata.js
@@ -0,0 +1,759 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State Description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Add location markers using latitude and longitude!",
+    location_color: "#2041D4",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "/static/lib/simplemaps/map_images/",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    AK: {
+      name: "Alaska",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    FL: {
+      name: "Florida",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    NH: {
+      name: "New Hampshire",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#New_Hampshire",
+      description: "Red-Spotted Newt"
+    },
+    VT: {
+      name: "Vermont",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Vermont",
+      description: "Northern Leopard Frog"
+    },
+    ME: {
+      name: "Maine",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    RI: {
+      name: "Rhode Island",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    NY: {
+      name: "New York",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#New_York",
+      description: "Wood Frog"
+    },
+    PA: {
+      name: "Pennsylvania",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    NJ: {
+      name: "New Jersey",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    DE: {
+      name: "Delaware",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    MD: {
+      name: "Maryland",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    VA: {
+      name: "Virginia",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    WV: {
+      name: "West Virginia",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    OH: {
+      name: "Ohio",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Ohio",
+      description: "American Bullfrog & Spotted Salamander"
+    },
+    IN: {
+      name: "Indiana",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    IL: {
+      name: "Illinois",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Illinois",
+      description: "Eastern Tiger Salamander"
+    },
+    CT: {
+      name: "Connecticut",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    WI: {
+      name: "Wisconsin",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    NC: {
+      name: "North Carolina",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#North_Carolina",
+      description: "Pine Barrens Tree Frog & Marbled Salamander"
+    },
+    DC: {
+      name: "District of Columbia",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    MA: {
+      name: "Massachusetts",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    TN: {
+      name: "Tennessee",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Tennessee",
+      description: "Tennessee Cave Salamander"
+    },
+    AR: {
+      name: "Arkansas",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    MO: {
+      name: "Missouri",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Missouri",
+      description: "American Bullfrog"
+    },
+    GA: {
+      name: "Georgia",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Georgia",
+      description: "American Green Tree Frog"
+    },
+    SC: {
+      name: "South Carolina",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#South_Carolina",
+      description: "Spotted Salamander"
+    },
+    KY: {
+      name: "Kentucky",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    AL: {
+      name: "Alabama",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Alabama",
+      description: "Red Hills Salamander"
+    },
+    LA: {
+      name: "Louisiana",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Louisana",
+      description: "American Green Tree Frog"
+    },
+    MS: {
+      name: "Mississippi",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    IA: {
+      name: "Iowa",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Iowa",
+      description: "American Bullfrog"
+    },
+    MN: {
+      name: "Minnesota",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Minnesota",
+      description: "Northern Leopard Frog"
+    },
+    OK: {
+      name: "Oklahoma",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Oklahoma",
+      description: "American Bullfrog"
+    },
+    TX: {
+      name: "Texas",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Texas",
+      description: "Texas Toad"
+    },
+    NM: {
+      name: "New Mexico",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      description: "New Mexico Spadefoot Toad",
+      url: "amphibians.html#New_Mexico"
+    },
+    KS: {
+      name: "Kansas",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Kansas",
+      description: "Barred Tiger Salamander"
+    },
+    NE: {
+      name: "Nebraska",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    SD: {
+      name: "South Dakota",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    ND: {
+      name: "North Dakota",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    WY: {
+      name: "Wyoming",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    MT: {
+      name: "Montana",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    CO: {
+      name: "Colorado",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Colorado",
+      description: "Western Tiger Salamander"
+    },
+    UT: {
+      name: "Utah",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    AZ: {
+      name: "Arizona",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Arizona",
+      description: "Arizona Tree Frog"
+    },
+    NV: {
+      name: "Nevada",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    OR: {
+      name: "Oregon",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    WA: {
+      name: "Washington",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      url: "amphibians.html#Washington",
+      description: "Pacific Tree Frog"
+    },
+    CA: {
+      name: "California",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      description: "California Red Legged Frog",
+      url: "amphibians.html#California"
+    },
+    MI: {
+      name: "Michigan",
+      color: "#a8f5a6",
+      hover_color: "#a8f5a6"
+    },
+    ID: {
+      name: "Idaho",
+      color: "#17ad12",
+      hover_color: "#1fbcdb",
+      description: "Idaho Giant Salamander",
+      url: "amphibians.html#Idaho"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      color: "#a8f5a6"
+    },
+    AS: {
+      name: "American Samoa",
+      hide: "yes"
+    }
+  },
+  locations: {},
+  labels: {
+    NH: {
+      parent_id: "NH",
+      x: "932",
+      y: "183",
+      pill: "yes",
+      width: 45,
+      display: "all",
+      color: "#000000"
+    },
+    VT: {
+      parent_id: "VT",
+      x: "883",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all",
+      color: "#000000"
+    },
+    RI: {
+      parent_id: "RI",
+      x: "932",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all",
+      color: "#000000"
+    },
+    NJ: {
+      parent_id: "NJ",
+      x: "883",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all",
+      color: "#000000"
+    },
+    DE: {
+      parent_id: "DE",
+      x: "883",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all",
+      color: "#000000"
+    },
+    MD: {
+      parent_id: "MD",
+      x: "932",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all",
+      color: "#000000"
+    },
+    DC: {
+      parent_id: "DC",
+      x: "884",
+      y: "332",
+      pill: "yes",
+      width: 45,
+      display: "all",
+      color: "#000000"
+    },
+    MA: {
+      parent_id: "MA",
+      x: "932",
+      y: "213",
+      pill: "yes",
+      width: 45,
+      display: "all",
+      color: "#000000"
+    },
+    CT: {
+      parent_id: "CT",
+      x: "932",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all",
+      color: "#000000"
+    },
+    HI: {
+      parent_id: "HI",
+      x: 305,
+      y: 565,
+      pill: "yes",
+      color: "#000000"
+    },
+    AK: {
+      parent_id: "AK",
+      x: "113",
+      y: "495",
+      color: "#000000"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "773",
+      y: "510",
+      color: "#000000"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "893",
+      y: "85",
+      color: "#000000"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "815",
+      y: "158",
+      color: "#000000"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "786",
+      y: "210",
+      color: "#000000"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "790",
+      y: "282",
+      color: "#000000"
+    },
+    WV: {
+      parent_id: "WV",
+      x: "744",
+      y: "270",
+      color: "#000000"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "700",
+      y: "240",
+      color: "#000000"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "650",
+      y: "250",
+      color: "#000000"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "600",
+      y: "250",
+      color: "#000000"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "575",
+      y: "155",
+      color: "#000000"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "784",
+      y: "326",
+      color: "#000000"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "655",
+      y: "340",
+      color: "#000000"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "548",
+      y: "368",
+      color: "#000000"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "548",
+      y: "293",
+      color: "#000000"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "718",
+      y: "405",
+      color: "#000000"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "760",
+      y: "371",
+      color: "#000000"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "680",
+      y: "300",
+      color: "#000000"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "655",
+      y: "405",
+      color: "#000000"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "550",
+      y: "435",
+      color: "#000000"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "600",
+      y: "405",
+      color: "#000000"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "525",
+      y: "210",
+      color: "#000000"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "506",
+      y: "124",
+      color: "#000000"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "460",
+      y: "360",
+      color: "#000000"
+    },
+    TX: {
+      parent_id: "TX",
+      x: "425",
+      y: "435",
+      color: "#000000"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "305",
+      y: "365",
+      color: "#000000"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "445",
+      y: "290",
+      color: "#000000"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "420",
+      y: "225",
+      color: "#000000"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "413",
+      y: "160",
+      color: "#000000"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "416",
+      y: "96",
+      color: "#000000"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "300",
+      y: "180",
+      color: "#000000"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "280",
+      y: "95",
+      color: "#000000"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "320",
+      y: "275",
+      color: "#000000"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "223",
+      y: "260",
+      color: "#000000"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "205",
+      y: "360",
+      color: "#000000"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "140",
+      y: "235",
+      color: "#000000"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "100",
+      y: "120",
+      color: "#000000"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "130",
+      y: "55",
+      color: "#000000"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "200",
+      y: "150",
+      color: "#000000"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "79",
+      y: "285",
+      color: "#000000"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "663",
+      y: "185",
+      color: "#000000"
+    },
+    PR: {
+      parent_id: "PR",
+      x: "620",
+      y: "545"
+    },
+    GU: {
+      parent_id: "GU",
+      x: "550",
+      y: "540"
+    },
+    VI: {
+      parent_id: "VI",
+      x: "680",
+      y: "519"
+    },
+    MP: {
+      parent_id: "MP",
+      x: "570",
+      y: "575"
+    },
+    AS: {
+      parent_id: "AS",
+      x: "665",
+      y: "580"
+    }
+  },
+  regions: {}
+};
